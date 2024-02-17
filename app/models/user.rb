b4969cd10eaa981a70ba9b2495566662c5b12001
MayPyone <maypyone015@gmail.com>
2024-02-17 09:58:15 +0630

    Add authorization and migrations

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :reservations;
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  validates_presence_of :name, :email
+  validates :email, uniqueness: true
+end

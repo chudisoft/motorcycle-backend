762dbc9b4517bfde5322f52e304e8bcad4d72da9
MayPyone <maypyone015@gmail.com>
2024-02-19 01:45:35 +0630

    Delete the item

--- app/controllers/api/v1/motorcycles_controller.rb
+++ app/controllers/api/v1/motorcycles_controller.rb
@@ -1,7 +1,30 @@
 class Api::V1::MotorcyclesController < ApplicationController
   before_action :authorize_request
+  before_action :set_motorcycle, only: [:show, :destroy]
+
+  def index
+    motorcycles = Motorcycle.all
+    if motorcycles.empty?
+      render json: { message: "No motorcycles found", motorcycles: motorcycles }
+    else
+      render json: { message: "Motorcycles found", motorcycles: motorcycles }
+    end
+  end
+
   def show
     @motorcycle = Motorcycle.find(params[:id])
     render json: @motorcycle
+  end
+
+  def destroy
+    if @motorcycle.destroy
+      render json: { success: true, message: 'Motorcycle was deleted ' }
+    else
+      render json: { error: true, message: 'Failed to delete motorcycle' }, status: :unprocessable_entity
+    end
+  end
+
+  def set_motorcycle
+    @motorcycle = Motorcycle.find(params[:id])
   end
 end

80aa685f448b57fa9ce4894e25709d36ce5465d3
Christopher Uduekwe <christopheruduekwe@gmail.com>
2024-02-22 08:11:17 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  },
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
 
   get "user_profile", to: "members#index"
   get "motorcycles_available", to: "api/v1/motorcycles#motorcycles_available"
 
   namespace :api do
     namespace :v1 do
+      devise_for :users, path: '', path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      },
+      controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+      }
       resources :motorcycles do
         resources :reservations, only: [:create]
         member do
